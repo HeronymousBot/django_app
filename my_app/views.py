ca36a2a3b159d32b07a57d835f609c8840d4233a
Lorenzo Fonseca <lorenzo_moreira@hotmail.com.br>
2019-09-17 23:46:58 -0300

    initializing templates

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
-#MTV
-#M -Models
-#T - Templates
-#V - Views
+# MTV
+# M -Models
+# T - Templates
+# V - Views
 
 def home(request):
     return HttpResponse('Olá, mundo!')
 
+
 def home_param(request, post_id):
     return HttpResponse('Olá mundo!! %s' % post_id)
+
+
+def post_list(request):
+    name = 'Lorenzo'
+    return render(request, 'post_list.html', {'name': name})
